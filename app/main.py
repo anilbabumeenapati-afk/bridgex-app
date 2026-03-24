3dfdfb9e8ddc615a2f616c276500bc86b7899617
anilbabumeenapati-afk <anilbabu.meenapati@gmail.com>
2026-03-24 20:11:32 +0530

    fix production api

--- app/main.py
+++ app/main.py
@@ -30,7 +30,7 @@ def download_file(filename: str):
 # ✅ CORS FIX
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # React dev server
+    allow_origins=["*"],  # React dev server
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
