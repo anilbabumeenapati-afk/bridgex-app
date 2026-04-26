6265a8bfbb24e89902988aacad7f9a307ad26533
anilbabumeenapati-afk <anilbabu.meenapati@gmail.com>
2026-04-26 14:46:06 +0530

    cors issue resolving

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://bridgex-app-git-main-anilbabuaflcrs-projects.vercel.app"
+        "https://bridgex-app-git-main-anilbabuaflcrs-projects.vercel.app",
+        "https://bridgex-app.vercel.app"
     ],
     allow_credentials=False,
     allow_methods=["*"],
